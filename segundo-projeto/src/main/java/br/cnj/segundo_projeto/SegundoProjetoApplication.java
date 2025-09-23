a78ee7435877743d0833aa1a244d2567e3902f3e
tr302259 <vinicius.caixeta@trf1.jus.br>
2025-09-23 11:27:00 -0300

    Criado projeto spring utilizando spring initializr

---
+++ segundo-projeto/src/main/java/br/cnj/segundo_projeto/SegundoProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.cnj.segundo_projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoProjetoApplication.class, args);
+	}
+
+}
---
+++ segundo-projeto/src/test/java/br/cnj/segundo_projeto/SegundoProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.cnj.segundo_projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
